5428f4a95a74b2cb6ced97d521081a967d329157
Nicola Trentini <nicola.trentini.wild@gmail.com>
2022-07-10 19:33:57 +0200

    setted scss

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HiUser msg="Salut, Lucile" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HiUser from './components/HiUser.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HiUser
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './sass/general.scss';
+
 </style>
---
+++ src/components/HiUser.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="helloMessage">
+   <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HiUser',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+
+</style>
